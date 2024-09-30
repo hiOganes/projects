3033228d778d65f67367102c2be98987d714b66d
Oganes <123650676+hiOganes@users.noreply.github.com>
2024-09-30 17:10:38 +0300

    new

--- asm_website/asm/main_pages/views.py
+++ asm_website/asm/main_pages/views.py
@@ -14,3 +14,17 @@ class PortfolioPage(TemplateView):
 class ContactsPage(TemplateView):
     template_name = 'main_pages/contacts.html'
 
+class LogosCorpPage(TemplateView):
+    template_name = 'main_pages/logos_corp.html'
+
+class StickersPage(TemplateView):
+    template_name = 'main_pages/stickers.html'
+
+class IllustrationsPage(TemplateView):
+    template_name = 'main_pages/illustrations.html'
+
+class CardsPage(TemplateView):
+    template_name = 'main_pages/cards.html'
+
+class AnimationPage(TemplateView):
+    template_name = 'main_pages/animation.html'
